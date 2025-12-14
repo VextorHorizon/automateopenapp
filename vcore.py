f1d25f51c4233472002416bce352cd32e895553d
VextorHorizon <Coptersrichanth@gmail.com>
2025-12-14 17:20:57 +0700

    small update/add loading of app data from profile.json
    Load application data from profile.json for further checks.

--- vcore.py
+++ vcore.py
@@ -3,3 +3,9 @@ import os
 import subprocess
 import webbrowser
 
+with open('profile.json', 'r') as file:
+    app_data = json.load(file)
+
+# print(app_data)
+
+# ตอนนี้มันรู้แล้วว่ามีอะไรบ้าง(ด้วยการเข้าไปอ่านใน json) สเต็ปต่อไปคือ 1.เช็คว่าเจ้าตัวแอพใน profile มันมีอยู่ในเครื่องจริงๆหรือป่าว 2.ถ้ามี ก้บอกว่ามี 3.เปิด 4.กลับไปวนใหม่เช็คตามขั้นลำดับ 
